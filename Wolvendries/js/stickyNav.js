99acc16ab0a72301312296bbd5015b0064a901cd
Jonas Van Obbergen <jonasvanobbergen@gmail.com>
2016-12-05 14:56:49 +0100

    Add files via upload
    Nieuwe pagina's toegevoegd ( Paarden, Infrastructuur). Op de home page javaScript toegevoegd voor het scrollen, bij infrastructuur de lichting box toegevoegd.

--- Wolvendries/js/stickyNav.js
+++ Wolvendries/js/stickyNav.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-var stickyNavTop = $('#container').offset().top;
+var stickyNavTop = $('#banner a').offset().top;
  
 var stickyNav = function(){
 var scrollTop = $(window).scrollTop();
       
 if (scrollTop > stickyNavTop) { 
     $('nav').addClass('sticky');
-    $("nav").css("animation-name" , "navigatie");
-    $("nav").css("animation-duration" , "0.8s");
 } else {
     $('nav').removeClass('sticky');
-    $("nav").css("position" , "fixt");
 }
 };
  
